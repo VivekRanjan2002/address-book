5f9d1e92205ff1aba381bf1eb7cfe692944613c6
VivekRanjan2002 <lit2020047@iiitl.ac.in>
2023-11-25 19:03:01 +0530

    Added:Contact Class Created

---
+++ Contact.java
@@ -0,0 +1,23 @@
+   public class Contact {
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String zip;
+    private String phoneNo;
+    private String emailID;
+
+    // constructor
+    public Contact(String firstName, String lastName, String address, String city,
+            String zip, String phoneNo, String emailID) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.zip = zip;
+        this.phoneNo = phoneNo;
+        this.emailID = emailID;
+
+    }
+ 
+}
