9a26403de61a6bd2f0a66299ce99b478061abc6c
VivekRanjan2002 <lit2020047@iiitl.ac.in>
2023-11-25 21:14:46 +0530

    Added: feature to edit contact detail

--- AddressBook.java
+++ AddressBook.java
@@ -10,9 +10,10 @@ public class AddressBook {
         HashMap<String, Contact> addressBook = new HashMap<>();
         boolean running = true;
         while (running) {
-            System.out.println("Press 1 to add the New Contact: ");
-            System.out.println("Press 2 to display the details by providing name: ");
-            System.out.println("Press 3 to exit");
+            System.out.println("Press 1 to add the New Contact ");
+            System.out.println("Press 2 to display the details by providing name ");
+            System.out.println("Press 3 to edit contact details");
+            System.out.println("Press 4 to exit");
             System.out.println("Please Enter your Option: ");
             int option = sc.nextInt();
             switch (option) {
@@ -22,11 +23,15 @@ public class AddressBook {
 
                     break;
                 case 2:
-                displayContact(addressBook);
+                    displayContact(addressBook);
                     break;
                 case 3:
+                    editDetails(addressBook);
+                    break;
+                case 4:
                     System.out.println("Program is Closing....");
                     running = false;
+                    break;
                 default:
                     System.out.println("Invalid Input");
                     break;
@@ -35,6 +40,7 @@ public class AddressBook {
         }
     }
 
+    // add new contact in addressbook
     public static void addNewContact(HashMap<String, Contact> addressBook) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Please Enter First Name: ");
@@ -53,26 +59,110 @@ public class AddressBook {
         String emailID = sc.nextLine();
 
         Contact C1 = new Contact(firstName, lastName, address, city, zip, PhoneNo, emailID);
-        String fullName = firstName + lastName;
-        addressBook.put(fullName, C1);
-        System.out.println(addressBook.get(fullName));
-        
+        String fullName = (firstName + lastName).replaceAll(" ", "");
+        System.out.println(fullName);
+
+        addressBook.put(fullName.toUpperCase(), C1);
+        System.out.println(addressBook.get(fullName.toUpperCase()));
+
         return;
     }
- 
-    public static void displayContact(HashMap<String,Contact> addressBook){
-        Scanner sc= new Scanner(System.in);
-        System.out.println("Please Enter First Name: ");
-        String firstName = sc.nextLine();
-        System.out.println("Please Enter Last Name : ");
-        String lastName = sc.nextLine();
-        String fullName = firstName + lastName; // to be searched in map;
-        if (addressBook.containsKey(fullName)) { 
-            System.out.println(addressBook.get(fullName));
-        }
-        else {
+
+    // display contact info from address by fullname as key
+    public static void displayContact(HashMap<String, Contact> addressBook) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please Enter Full Name: ");
+        String fullName = sc.nextLine();
+
+        if (addressBook.containsKey(fullName.replaceAll(" ", "").toUpperCase())) {
+            System.out.println(addressBook.get(fullName.toUpperCase()));
+        } else {
             System.out.println("OOPS! SORRY!! No such contact exist ! ");
         }
         return;
+    }
+
+    // edit contact details for particular fullname user
+    public static void editDetails(HashMap<String, Contact> addressBook) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please Enter Full Name: ");
+        String fullName = sc.nextLine();
+        String searchKey = fullName.replaceAll(" ", "").toUpperCase();
+        if (addressBook.containsKey(searchKey)) {
+            Contact contact = addressBook.get(searchKey);
+            addressBook.remove(searchKey);
+            boolean running = true;
+            while (running) {
+                System.out.println("Press 1 to edit First Name");
+                System.out.println("Press 2 to edit Last Name");
+                System.out.println("Press 3 to edit Address");
+                System.out.println("Press 4 to edit City");
+                System.out.println("Press 5 to edit Zip");
+                System.out.println("Press 6 to edit Phone No.");
+                System.out.println("Press 7 to edit Email ID");
+                System.out.println("Press 8 to confirm ");
+                System.out.println("Press your Option: ");
+
+                int option = sc.nextInt();
+
+                switch (option) {
+                    case 1:
+                        System.out.println("Please Enter Your New First Name");
+                        sc.nextLine();
+                        String firstName = sc.nextLine();
+                        contact.setFirstName(firstName);
+                        break;
+                    case 2:
+                        System.out.println("Please Enter Your New Last Name");
+                        sc.nextLine();
+                        String lastName = sc.nextLine();
+                        contact.setLastName(lastName);
+                        break;
+                    case 3:
+                        System.out.println("Please Enter Your New Address");
+                        sc.nextLine();
+                        String address = sc.nextLine();
+                        contact.setAddress(address);
+
+                        break;
+                    case 4:
+                        System.out.println("Please Enter Your New City");
+                        sc.nextLine();
+                        String city = sc.nextLine();
+                        contact.setCity(city);
+                        break;
+                    case 5:
+                        System.out.println("Please Enter Your New Zip");
+                        sc.nextLine();
+                        String zip = sc.nextLine();
+                        contact.setZip(zip);
+                        break;
+                    case 6:
+                        System.out.println("Please Enter Your New Phone No.");
+                        sc.nextLine();
+                        String phoneNo = sc.nextLine();
+                        contact.setPhoneNo(phoneNo);
+                        break;
+                    case 7:
+                        System.out.println("Please Enter Your New EmailID");
+                        sc.nextLine();
+                        String emailID = sc.nextLine();
+                        contact.setEmailId(emailID);
+                        break;
+                    case 8:
+                        String key = contact.getFirstName() + contact.getLastName();
+                        System.out.println(key);
+                        addressBook.put(key, contact);
+                        System.out.println("Contact updated Successfully");
+                        running = false;
+
+                    default:
+                        System.out.println("Please Enter Valid Option");
+                        break;
+                }
+            }
+        } else {
+            System.out.println("No such contact exist ");
+        }
     }
 }
--- Contact.java
+++ Contact.java
@@ -10,18 +10,77 @@
     // constructor
     public Contact(String firstName, String lastName, String address, String city,
             String zip, String phoneNo, String emailID) {
-        this.firstName = firstName;
-        this.lastName = lastName;
+        this.firstName = firstName.replace(" ","").toUpperCase(); // store in uppercase and erase all whitespace 
+        this.lastName = lastName.replace(" ","").toUpperCase(); 
         this.address = address;
-        this.city = city;
-        this.zip = zip;
-        this.phoneNo = phoneNo;
-        this.emailID = emailID;
+        this.city = city.replace(" ","").toUpperCase();
+        this.zip = zip.replace(" ","").toUpperCase();
+        this.phoneNo = phoneNo.replace(" ","").toUpperCase();
+        this.emailID = emailID.replace(" ","").toLowerCase();
 
     }
+     // getter function
+     public String getFirstName() {
+         return this.firstName;
+     }
+
+     public String getLastName(){
+        return this.lastName; 
+     }
+
+     public String getAddress() {
+         return this.address;
+     }
+
+     public String getCity(){
+        return this.city;
+     }
+
+     public String getZip()
+     {
+         return this.zip;
+     }
+
+     public String getPhoneNo(){
+        return this.phoneNo;
+     }
+
+     public String getEmailId() {
+         return this.emailID;
+     }
  
-      /*
-      If we print any object,Java compiler  internally invokes the toString() method on the object.
+      // setter function
+     public void setFirstName(String firstName) {
+          this.firstName=firstName.replace(" ","").toUpperCase();
+     }
+
+      public void setLastName(String lastName){
+         this.lastName=lastName.replace(" ","").toUpperCase(); 
+     }
+
+     public void setAddress(String address) {
+          this.address=address;
+     }
+
+     public void setCity(String city){
+         this.city=city.replace(" ","").toUpperCase();
+     }
+
+     public void setZip(String zip)
+     {
+          this.zip=zip.replace(" ","").toUpperCase();
+     }
+
+     public void setPhoneNo(String phoneNo){
+         this.phoneNo=phoneNo.replace(" ","").toUpperCase();
+     }
+
+     public void setEmailId(String emailID){
+         this.emailID=emailID.replace(" ","").toLowerCase();
+     }
+ 
+    /*
+     If we print any object,Java compiler  internally invokes the toString() method on the object.
        So overriding the toString() method, returns the desired output
      */
     // @Override
